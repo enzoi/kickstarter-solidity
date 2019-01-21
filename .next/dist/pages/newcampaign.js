8619695be72211e07a15071bff3c647647678d5f
Yeontae Kim <ytk007@gmail.com>
2019-01-20 16:44:30 -0800

    Frontend setup with nextJS

---
+++ .next/dist/pages/newcampaign.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/yeontaekim/ytk/Web/BlockChain/kickstart/pages/newcampaign.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, 'This is the new campaign page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL25ld2NhbXBhaWduLmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFESixBQUNJLEFBRVA7QUFKRCIsImZpbGUiOiJuZXdjYW1wYWlnbi5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMveWVvbnRhZWtpbS95dGsvV2ViL0Jsb2NrQ2hhaW4va2lja3N0YXJ0In0=
