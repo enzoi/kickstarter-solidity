5eff152f8de595f29abb96351dde100e94c8e1da
Yeontae Kim <ytk007@gmail.com>
2019-01-21 00:15:06 -0800

    Create new campaign page with css fix

---
+++ pages/campaigns/new.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+class CampaignNew extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <h1>New Campaign!!</h1>
+            </Layout>
+        );
+    }
+}
+
+export default CampaignNew;
