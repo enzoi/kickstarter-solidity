3b4d4df719a873cd2a5e028bf7a15eac1916a671
Yeontae Kim <ytk007@gmail.com>
2019-01-21 23:22:54 -0800

    Update request related functionalities

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,12 +3,14 @@ import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends React.Component {
     static async getInitialProps(props) {
         const { address } = props.query;
         const campaign = Campaign(address);
         const requestCount = await campaign.methods.getRequestsCount().call();
+        const approversCount = await campaign.methods.approversCount().call();
 
         const requests = await Promise.all(
             Array(parseInt(requestCount))
@@ -18,10 +20,23 @@ class RequestIndex extends React.Component {
             })
         );
 
-        console.log(requests);
-
-        return { address, requests, requestCount }
+        return { address, requests, requestCount, approversCount }
     }
+
+    renderRows() {
+        return this.props.requests.map((request, index) => {
+            return (
+                <RequestRow 
+                    key={index}
+                    id={index}
+                    request={request}
+                    address={this.props.address}
+                    approversCount={this.props.approversCount}
+                />
+            );
+        });
+    }
+
     render() {
         const { Header, Row, HeaderCell, Body } = Table;
 
@@ -45,6 +60,9 @@ class RequestIndex extends React.Component {
                             <HeaderCell>Finalize</HeaderCell>
                         </Row>
                     </Header>
+                    <Body>
+                        {this.renderRows()}
+                    </Body>
                 </Table>
             </Layout>
             
